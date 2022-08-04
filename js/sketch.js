c0064e12c9300d8fcbceb85018413ed896897861
Yuto02D2-E2 <d.yuto.k.b@gmail.com>
2022-08-05 02:41:37 +0900

    [save/fixme] it contains a serious bug

--- js/sketch.js
+++ js/sketch.js
@@ -19,7 +19,8 @@ function setup() {
 
 
 function draw() {
-    // 1[sec]毎に呼ばれる．背景を描画してから，盤を描画する
+    // 1/60[sec]毎に呼ばれる．背景を描画してから，盤を描画する
+    clear(); // 画面を一旦クリア．これが無いとどんどん上書きされて色が濃くなってしまう
     game.draw();
 }
 
@@ -33,7 +34,10 @@ function windowResized() {
 
 function mouseClicked() {
     // マウスをクリックして離したタイミングで呼ばれる．盤の情報を更新する
-    game.update();
+    const [y, x] = game.pos_to_idx(mouseY, mouseX);
+    if (game.is_board_inside(y, x)) {
+        game.update(mouseY, mouseX);
+    }
 }
 
 
@@ -44,45 +48,51 @@ function keyPressed() {
             game.init();
         }
     } else {
-        window.alert("キーボードからの入力を検知しました．[r]キーでゲームを初期化できます．");
+        game.help();
     }
 }
 
 
 // -----------------------------------------------------------------------------------------------------------------
-// 以下クラスの定義．本当は別ファイルに切り出したいけど，p5.jsだと無理っぽい
+// 以下クラスの定義．本当は別ファイルに切り出したいけど，p5.jsだと無理っぽい?
 
 
 class Game {
     constructor() {
         this.STONE = { // JSにはEnumが無いらしいので，連想配列？で仮想的に再現する
-            SPACE: color(127, 255, 127),
-            // SPACE: color("hsba(160, 100%, 50%, 0.5)"),
             WHITE: color(255, 255, 255),
             BLACK: color(0, 0, 0),
+            SPACE: color(127, 255, 127),
+            // SPACE: color("hsba(160, 100%, 50%, 0.5)"),
+            // AVAILABLE: color("rgba(255,127,255,0.5)"),
+            // AVAILABLE: color("#ffccff"),
         };
-        this.BOARD_SIZE = 9;
+        this.BOARD_SIZE = 700;
+        // TODO: 700の部分をwindow.innnerWidth/Height等を使ってレスポンシブ対応する
+        // PCなら700, タブレットなら??, スマホなら??に設定する
+        this.CELL_NUM = 8; // 8 + 2(padding)
+        this.CELL_SIZE = this.BOARD_SIZE / this.CELL_NUM;
         this.board = Array.from( // create 2D array (initialize value: this.stone.space)
-            new Array(this.BOARD_SIZE), _ => new Array(this.BOARD_SIZE).fill(this.STONE.SPACE)
+            new Array(this.CELL_NUM + 2), _ => new Array(this.CELL_NUM + 2).fill(this.STONE.SPACE)
         );
         // console.info(this.board);
         // MEMO: debug用
         this.mode = "Single";
         this.player = [
-            new User("user", this.STONE.BLACK), // 先攻が黒らしい
-            new Computer("computer", this.STONE.WHITE)
+            new User("user", this.STONE.BLACK, this.CELL_NUM), // 先攻が黒らしい
+            new Computer("computer", this.STONE.WHITE, this.CELL_NUM)
         ];
         // if (window.confirm("モードを選択してください．\nプレイ人数は1人ですか？")) {
         //     this.mode = "Single";
         //     this.player = [
-        //         new User("user"),
-        //         new Computer("computer")
+        //         new User("user", this.STONE.BLACK),
+        //         new Computer("computer", this.STONE.WHITE)
         //     ];
         // } else {
         //     this.mode = "Multi";
         //     this.player = [
-        //         new User("player1"),
-        //         new User("player2")
+        //         new User("player1", this.STONE.BLACK),
+        //         new User("player2", this.STONE.WHITE)
         //     ];
         // }
         this.player_idx = 0;
@@ -91,8 +101,8 @@ class Game {
     init() {
         console.log("[INFO] sketch.js/Game/game.init() is called");
         // init board
-        for (let y = 0; y < this.BOARD_SIZE; y++) {
-            for (let x = 0; x < this.BOARD_SIZE; x++) {
+        for (let y = 1; y <= this.CELL_NUM; y++) {
+            for (let x = 1; x <= this.CELL_NUM; x++) {
                 if (((y === 4) && (x === 4)) || ((y === 5) && (x === 5))) {
                     this.board[y][x] = this.STONE.WHITE;
                 } else if (((y === 4) && (x === 5)) || ((y === 5) && (x === 4))) {
@@ -102,51 +112,147 @@ class Game {
                 }
             }
         }
+        // console.info(this.board);
         // init player
         this.player_idx = 0;
+        this.check_available();
     }
 
     draw() {
-        background(this.STONE.SPACE);
-        for (let y = 1; y < this.BOARD_SIZE; y++) {
-            for (let x = 1; x < this.BOARD_SIZE; x++) {
-                // マス目
+        background(this.STONE.WHITE);
+        for (let y = 1; y <= this.CELL_NUM; y++) {
+            for (let x = 1; x <= this.CELL_NUM; x++) {
+                // マスの格子線と背景色
                 stroke(0);
                 strokeWeight(2);
-                noFill();
-                // TODO: 800の部分をwindow.innnerWidth/Heightを使ってレスポンシブ対応する
-                // PCなら800, タブレットなら??, スマホなら??に設定する
-                const lx = x * 800 / this.BOARD_SIZE;
-                const ly = y * 800 / this.BOARD_SIZE;
-                square(lx, ly, lx / x); // 外枠; (lx,ly,size)
-                // 石
-                if (this.board[y][x] === this.STONE.SPACE) continue;
-                noStroke();
-                fill(this.board[y][x]);
-                const r = (lx / x) * 0.8; // マス目の8割のサイズ
+                fill(this.STONE.SPACE);
+                const lx = x * this.CELL_SIZE, ly = y * this.CELL_SIZE;
+                square(lx, ly, this.CELL_SIZE); // 外枠; (lx,ly,size)
+                if (this.board[y][x] === this.STONE.SPACE) {
+                    if (this.player[this.player_idx].availables.includes([y, x])) {
+                        // 置ける場所をハイライト
+                        stroke(255, 127, 255, 0.5);
+                        noFill();
+                    } else {
+                        continue;
+                    }
+                } else {
+                    // 石を描画
+                    noStroke();
+                    fill(this.board[y][x]);
+                }
+                const r = this.CELL_SIZE * 0.8; // 円のサイズをマス目の8割のサイズにする
                 ellipse(lx + r * 0.1, ly + r * 0.1, r); // (lx,ly,r)
             }
         }
     }
 
-    update() {
+    update(mouse_y, mouse_x) {
         console.log("[INFO] sketch.js/Game/game.update() is called");
-        this.player[this.player_idx].check_available();
-        this.player[this.player_idx].put_stone();
-        this.player[this.player_idx].flip_stone();
-        this.player[this.player_idx].update_score();
+        const [y, x] = this.pos_to_idx(mouse_y, mouse_x); // unpack
+        if (!this.put_stone(y, x)) {
+            console.log("その場所に置くことは出来ません．\nハイライトされている部分のみ置くことが出来ます");
+            // window.alert("その場所に置くことは出来ません．\nハイライトされている部分のみ置くことが出来ます");
+            return;
+        }
+        // this.player[this.player_idx].flip_stone();
+        // this.player[this.player_idx].update_score();
         this.player_idx ^= 1;
-
-        // ゲームの終了判定
-        // if (end) {
-        //     this.ending();
-        // }
+        if (this.check_available() === 0) {
+            console.log("次のプレイヤーが打てる場所はありません．パスします");
+            // window.alert("次のプレイヤーが打てる場所はありません．パスします");
+            this.player_idx ^= 1;
+        }
+        if (is_game_over()) {
+            this.ending();
+        }
     }
 
     ending() {
         console.log("[INFO] sketch.js/Game/game.ending() is called");
         const name = this.player[this.player_idx].get_name();
         window.alert(`xx対ooで${name}の勝ちです\nもう一度遊びたい場合は，[r]キーを押してゲームをリセットして下さい．`);
+    }
+
+    help() {
+        window.alert(`
+        キーボードからの入力を検知しました．
+        [INFO]
+        1. r キーでゲームを初期化
+        2. その他のキーでヘルプを表示
+        できます．
+        `);
+    }
+
+    pos_to_idx(mouse_y, mouse_x) {
+        console.log(`[INFO] pos2idx:(mouseY,mouseX)=(${mouse_y},${mouse_x})`);
+        return [
+            Math.floor(mouse_y / this.CELL_SIZE),
+            Math.floor(mouse_x / this.CELL_SIZE),
+        ];
+    }
+
+    is_board_inside(y, x) { // y,x is index
+        if ((y < 0) || (this.CELL_NUM < y)) return false;
+        if ((x < 0) || (this.CELL_NUM < x)) return false;
+        return true;
+    }
+
+    put_stone(y, x) {
+        if (this.player[this.player_idx].availables.includes([y, x])) {
+            this.board[y][x] = this.player[this.player_idx].stone_color;
+            return true;
+        }
+        return false;
+    }
+
+    check_available() {
+        // 置ける場所をハイライト表示
+        // 置ける場所 := 相手の石を1つ以上挟むことが出来る場所
+        // もしも，打てる場所が一つも無い場合はパス．aleatを出す．
+        // 方針：場所を全探索して，置けるかどうか判定する．判定するために，8方向それぞれを見て，隣に別の色が居て，それ以降に同じ色が出てこればok
+        this.player[this.player_idx].availables.splice(0); // all clear
+        for (let y = 1; y < this.CELL_NUM; y++) {
+            for (let x = 1; x < this.CELL_NUM; x++) {
+                if ((this.board[y][x] === this.STONE.SPACE) && this.is_available(y, x)) {
+                    this.player[this.player_idx].availables.push([y, x]);
+                }
+            }
+        }
+        return this.player[this.player_idx].availables.length;
+    }
+
+    is_available(y, x) {
+        const directions = [ // (y, x). 上から時計回り
+            [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]
+        ];
+        for (let i = 0; i < directions.length; i++) {
+            const [dy, dx] = directions[i];
+            let line = new Array();
+            while (this.is_board_inside(y + dy, x + dx)) {
+                if (this.board[y + dy][x + dx] === this.STONE.SPACE) break;
+                line.push(this.board[y + dy][x + dx]);
+                if (this.board[y + dy][x + dx] === this.player[this.player_idx].stone_color) break;
+            }
+            // console.log(`(${y},${x},${y + dy},${x + dx}) -> line:${line}`);
+            if ((line[0] === this.player[this.player_idx ^ 1].stone_color) &&
+                (line[line.length - 1] === this.player[this.player_idx].stone_color)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    flip_stone() { }
+    update_score() { }
+
+    is_game_over() {
+        for (let y = 1; y < this.CELL_NUM; y++) {
+            for (let x = 1; x < this.CELL_NUM; x++) {
+                if (this.board[y][x] === this.STONE.SPACE) return false;
+            }
+        }
+        return true;
     }
 }
 
@@ -159,21 +265,6 @@ class Player {
         this.availables = new Array();
     }
 
-    check_available() {
-        // 置ける場所をハイライト表示
-        // 置ける場所 := 相手の石を1つ以上挟むことが出来る場所
-        // もしも，打てる場所が一つも無い場合はパス．aleatを出す．
-        // 置ける場所を探すのは，DFSすれば良さそう
-    }
-    put_stone() {
-        // check_available()で見つかった場所以外に置こうとするとaleatを出してもう一度考えさせる
-    }
-    flip_stone() { }
-    update_score() {
-        // 置いた時点で1つ以上ひっくり返せるのは確定しているので(ひっくり返せない場所には置けないから)
-        // スコアの更新も必ず発生する
-    }
-
     get get_name() {
         return this.name;
     }
@@ -184,9 +275,9 @@ class Player {
 
 
 class User extends Player {
-    constructor(name, stone_color) {
-        super(name, stone_color);
-    }
+    // constructor(name, stone_color, CELL_NUM) {
+    //     super(name, stone_color, CELL_NUM);
+    // }
 
     // override
     put_stone() {
@@ -195,9 +286,9 @@ class User extends Player {
 
 
 class Computer extends Player {
-    constructor(name, stone_color) {
-        super(name, stone_color);
-    }
+    // constructor(name, stone_color) {
+    //     super(name, stone_color);
+    // }
 
     // override
     put_stone() {
